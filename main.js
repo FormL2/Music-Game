8deae8cfd32477addb613df67a7b088f90ea44bf
FormL2 <106445130+FormL2@users.noreply.github.com>
2022-05-29 04:34:08 +0100

    May or may not have accidentally put this on crisslecrossle...

--- main.js
+++ main.js
@@ -1,24 +1,64 @@
-var song1 = new Audio("prnkl.mp3");
-var song2 = new Audio("prnkl.mp3");
-var numbers = [0,1,2]
+if (window.localStorage.getItem("unusedSongs") == null) {
+  var numbers = [0,1,2];
+  window.localStorage.setItem("unusedSongs",numbers);
+} else if ((window.localStorage.getItem("unusedSongs") == "")) {
+  var numbers = [];
+} else {
+  var numbers = window.localStorage.getItem("unusedSongs").split(",");
+};
+
+if (window.localStorage.getItem("currentLeft") == null) {
+  var number = Math.floor(Math.random()*numbers.length);
+  document.getElementById("left-img").src = images[numbers[number]];
+  document.getElementById("left-artist").innerHTML = artists[numbers[number]];
+  document.getElementById("left-title").innerHTML = titles[numbers[number]];
+  document.getElementById("left-audio").src = audios[numbers[number]];
+  window.localStorage.setItem("currentLeft",numbers[number]);
+  numbers.splice(number,1);
+  window.localStorage.setItem("unusedSongs",numbers)
+} else {
+  document.getElementById("left-img").src = images[window.localStorage.getItem("currentLeft")];
+  document.getElementById("left-artist").innerHTML = artists[window.localStorage.getItem("currentLeft")];
+  document.getElementById("left-title").innerHTML = titles[window.localStorage.getItem("currentLeft")];
+  document.getElementById("left-audio").src = audios[window.localStorage.getItem("currentLeft")];
+};
+
+if (window.localStorage.getItem("currentRight") == null) {
+  var number = Math.floor(Math.random()*numbers.length);
+  document.getElementById("right-img").src = images[numbers[number]];
+  document.getElementById("right-artist").innerHTML = artists[numbers[number]];
+  document.getElementById("right-title").innerHTML = titles[numbers[number]];
+  document.getElementById("right-audio").src = audios[numbers[number]];
+  window.localStorage.setItem("currentRight",numbers[number]);
+  numbers.splice(number,1);
+  window.localStorage.setItem("unusedSongs",numbers)
+} else {
+  document.getElementById("right-img").src = images[window.localStorage.getItem("currentRight")];
+  document.getElementById("right-artist").innerHTML = artists[window.localStorage.getItem("currentRight")];
+  document.getElementById("right-title").innerHTML = titles[window.localStorage.getItem("currentRight")];
+  document.getElementById("right-audio").src = audios[window.localStorage.getItem("currentRight")];
+};
 
 function update() {
   if (numbers.length == 0) {
     console.log("end")
+    document.getElementById("game").style = "display: none";
+    document.getElementById("game-over").style = "display: inherit";
+    document.getElementById("left-audio").pause();
+    document.getElementById("right-audio").pause();
   } else {
   var number = Math.floor(Math.random()*numbers.length);
   document.getElementById("right-img").src = images[numbers[number]];
   document.getElementById("right-artist").innerHTML = artists[numbers[number]];
   document.getElementById("right-title").innerHTML = titles[numbers[number]];
   document.getElementById("right-audio").src = audios[numbers[number]];
-
+  window.localStorage.setItem("currentRight",numbers[number]);
   numbers.splice(number,1);
-  }
-}
+  window.localStorage.setItem("unusedSongs",numbers);
+  };
+};
 
-update()
-
-document.getElementById('right-img').ondragstart = document.getElementById('left-img').ondragstart = () => {
+document.getElementById("right-img").ondragstart = document.getElementById("left-img").ondragstart = () => {
   return false;
 };
 
@@ -48,7 +88,7 @@ document.getElementById("right-img").onclick = () => {
     document.getElementById("right-audio").play();
     right_playing = true
   } else {
-    document.getElementById("right-audio").pause()
+    document.getElementById("right-audio").pause();
     document.getElementById("right-audio").currentTime = 0;
   };
 };
@@ -57,3 +97,17 @@ document.getElementById("right-audio").onpause = () => {
   right_playing = false;
 };
 
+document.getElementById("left-vote").onclick = () => {
+  left_playing = right_playing = false;
+  update();
+}
+
+document.getElementById("right-vote").onclick = () => {
+  left_playing = right_playing = false;
+  document.getElementById("left-img").src = document.getElementById("right-img").src;
+  document.getElementById("left-artist").innerHTML = document.getElementById("right-artist").innerHTML;
+  document.getElementById("left-title").innerHTML = document.getElementById("right-title").innerHTML;
+  document.getElementById("left-audio").src = document.getElementById("right-audio").src;
+  window.localStorage.setItem("currentLeft",window.localStorage.getItem("currentRight"));
+  update()
+}
