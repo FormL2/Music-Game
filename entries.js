6ecdd5e59a776f8f785a1281669092f991466e45
FormL2 <106445130+FormL2@users.noreply.github.com>
2022-05-30 20:13:04 +0100

    You like my Mozart album cover?

--- entries.js
+++ entries.js
@@ -4,7 +4,9 @@ const titles = [
     "The Wilhelm Scream (feat. Meghgan Washington)",
     "The Man",
     "Funky Cold Medina",
-]
+    "Don't Stop Believin'",
+    "Requiem Mass in D Minor, K. 626: III. Lacrimosa",
+];
 
 const artists = [
     "Imagine Dragons",
@@ -12,7 +14,9 @@ const artists = [
     "The Bamboos",
     "The Killers",
     "Tone-Lōc",
-]
+    "Journey",
+    "Mozart",
+];
 
 const images = [
     "Bones.webp",
@@ -20,7 +24,9 @@ const images = [
     "The-Wilhelm-Scream.jfif",
     "The-Man.jpg",
     "Funky-Cold-Medina.jpg",
-]
+    "Don't-Stop-Believin'.jpg",
+    "Lacrimosa.png",
+];
 
 const audios = [
     "Bones.mp3",
@@ -28,4 +34,6 @@ const audios = [
     "The-Wilhelm-Scream.mp3",
     "The-Man.mp3",
     "Funky-Cold-Medina.mp3",
-]
+    "Don't-Stop-Believin'.mp3",
+    "Lacrimosa.mp3",
+];
