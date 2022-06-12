f45056bd72c2cf980018102427c19cf6124b75ae
TrulyTHRiO <55783018+TrulyTHRiO@users.noreply.github.com>
2022-06-12 22:52:15 +0100

    Added some more songs and Leo versions.

---
+++ Images/entries.js
@@ -0,0 +1,59 @@
+const titles = [
+    "Bones",
+    "Broken Strings",
+    "The Wilhelm Scream (feat. Meghgan Washington)",
+    "The Man",
+    "Funky Cold Medina",
+    "Don't Stop Believin'",
+    "Requiem Mass in D Minor, K. 626: III. Lacrimosa",
+    "Boulevard of Broken Dreams",
+    "California Gurls (feat. Snoop Dogg)",
+    "Locomotive Breath",
+    "Seen It All",
+    "Touch The Sky (feat. Lupe Fiasco)",
+];
+
+const artists = [
+    "Imagine Dragons",
+    "James Morrison",
+    "The Bamboos",
+    "The Killers",
+    "Tone-Lōc",
+    "Journey",
+    "Mozart",
+    "Green Day",
+    "Katy Perry",
+    "Jethro Tull",
+    "Jake Bugg",
+    "Kanye West (Ye)",
+];
+
+const images = [
+    "Bones.webp",
+    "Broken-Strings.jpg",
+    "The-Wilhelm-Scream.jfif",
+    "The-Man.jfif",
+    "Funky-Cold-Medina.jpg",
+    "Don't-Stop-Believin'.jpg",
+    "Lacrimosa.png",
+    "Boulevard-of-Broken-Dreams.jpg",
+    "California-Gurls.jpg",
+    "Locomotive-Breath.jpg",
+    "Seen-It-All.jpg",
+    "Touch-The-Sky.jpg",
+];
+
+const audios = [
+    "Bones.mp3",
+    "Broken-Strings.mp3",
+    "The-Wilhelm-Scream.mp3",
+    "The-Man.mp3",
+    "Funky-Cold-Medina.mp3",
+    "Don't-Stop-Believin'.mp3",
+    "Lacrimosa.mp3",
+    "Boulevard-of-Broken-Dreams.mp3",
+    "California-Gurls.mp3",
+    "Locomotive-Breath.mp3",
+    "Seen-It-All.mp3",
+    "Touch-The-Sky.mp3",
+];
